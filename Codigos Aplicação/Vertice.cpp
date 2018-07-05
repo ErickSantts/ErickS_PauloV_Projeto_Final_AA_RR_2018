044e9a5166832d02ce0b53bfb9e1be8d6ff4c160
ErickSantts <erickcrazysantos@gmail.com>
2018-07-05 06:49:18 -0700

    Codigos Aplicação Sensores

---
+++ Codigos Aplicação/Vertice.cpp
@@ -0,0 +1,56 @@
+#include "../include/Vertice.h"
+#include <iostream>
+
+Vertice::Vertice (int id_v, int grau_n, int diferenca, double distancia, double mobilidade, double processamento) {
+	id = id_v;
+	grau = grau_n;
+	G_v = diferenca;
+	D_v = distancia;
+	M_v = mobilidade;
+	P_v = processamento;
+	W_v = 0;
+}
+
+void Vertice::calcularW_v (double v1, double v2, double v3, double v4) {
+	W_v = (G_v * v1) + (D_v * v2) + (M_v * v3) + (P_v * v4);
+}
+
+double Vertice::getW_v () {
+	return W_v;
+}
+
+double Vertice::getD_v () {
+	return D_v;
+}
+
+double Vertice::getP_v () {
+	return P_v;
+}
+
+double Vertice::getM_v () {
+	return M_v;
+}
+
+int Vertice::getId () {
+	return id;
+}
+
+void Vertice::setW_v (double v) {
+	
+	W_v = v;
+}
+
+void Vertice::setP_v (double v) {
+	
+	P_v = v;
+}
+
+void Vertice::setM_v (double v) {
+	
+	M_v = v;
+}
+
+void Vertice::setD_v (double v) {
+	
+	D_v = v;
+}
